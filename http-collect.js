23badc1fc2d2a1513fc16cc0c0ebe02ec70c1dc2
Taysir Oauslati <thessalyse@hotmail.fr>
2022-07-12 18:34:22 +0200

    first commit

---
+++ http-collect.js
@@ -0,0 +1,73 @@
+// 'use strict'
+// const http = require('http')
+// const bl = require('bl')
+
+// http.get(process.argv[2], function (response) {
+//   response.pipe(bl(function (err, data) {
+//     if (err) {
+//       return console.error(err)
+//     }
+//     data = data.toString()
+//     console.log(data.length)
+//     console.log(data)
+//   }))
+// })
+// **************************************
+// var http = require('http');
+// var bl = require('bl');
+// var url = process.argv[2];
+
+// http.get(url,function(response){
+//     response.setEncoding('utf8');
+    
+//     var sum = 0;
+//     var arr = [];
+//     response.pipe(bl(function(err,data){
+//             if (err)
+//             return console.error(err);
+            
+//             sum+=data.length;
+//             arr.push(data.toString());
+//         })
+//     );
+    
+//     response.on('end',function(){
+//         console.log(sum);
+//         arr.forEach(function(elem){
+//             console.log(elem);
+//         });
+//     });
+    
+//     response.on('error', console.error);
+    
+// });
+
+// Solution 1
+/*
+var http = require('http');
+var bl = require('bl');
+http.get(process.argv[2], function (response) {
+  response.pipe(bl(function (err, data) {
+    if (err)
+      return console.error(err);
+    data = data.toString();
+    console.log(data.length);
+    console.log(data);
+  }));
+});
+*/
+
+'use strict'
+const http = require('http')
+const bl = require('bl')
+
+http.get(process.argv[2], function (response) {
+  response.pipe(bl(function (err, data) {
+    if (err) {
+      return console.error(err)
+    }
+    data = data.toString()
+    console.log(data.length)
+    console.log(data)
+  }))
+})
